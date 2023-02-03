a601b6dd1d6209490934225bed8782063fe5996e
Alexa Bolin <anb71@duke.edu>
2023-02-03 13:08:47 -0500

    added testing for blood_calculator and fixed all python files to match pip8 style guide

--- blood_calculator.py
+++ blood_calculator.py
@@ -16,17 +16,20 @@ def interface():
             LDL_driver()
         elif choice == "3":
             Chol_driver()
-    print("Program ending") 
+    print("Program ending")
+
 
 def generic_input(test_name):
     test_value = input("Enter the {} results: ".format(test_name))
     test_value = int(test_value)
     return test_value
 
+
 def HDL_driver():
     HDL_in = generic_input("HDL")
     HDL_analy = HDL_analysis(HDL_in)
     HDL_output(HDL_in, HDL_analy)
+
 
 def HDL_analysis(HDL_int):
     if HDL_int >= 60:
@@ -37,14 +40,17 @@ def HDL_analysis(HDL_int):
         answer = "Low"
     return answer
 
+
 def HDL_output(HDL_value, HDL_analy):
     print("The HDL result of {} is considered {}".format(HDL_value, HDL_analy))
     return
+
 
 def LDL_driver():
     LDL_in = generic_input("LDL")
     LDL_analy = LDL_analysis(LDL_in)
     LDL_output(LDL_in, LDL_analy)
+
 
 def LDL_analysis(LDL_int):
     if LDL_int < 130:
@@ -57,14 +63,17 @@ def LDL_analysis(LDL_int):
         answer = "Very High"
     return answer
 
+
 def LDL_output(LDL_value, LDL_analy):
     print("The LDL result of {} is considered {}".format(LDL_value, LDL_analy))
     return
+
 
 def Chol_driver():
     Chol_in = generic_input(" total cholesterol")
     Chol_analy = Chol_analysis(Chol_in)
     Chol_output(Chol_in, Chol_analy)
+
 
 def Chol_analysis(Chol_int):
     if Chol_int >= 240:
@@ -75,9 +84,12 @@ def Chol_analysis(Chol_int):
         answer = "Normal"
     return answer
 
+
 def Chol_output(Chol_value, Chol_analy):
-    print("The total cholesterol result of {} is considered {}".format(Chol_value, Chol_analy))
+    print("The total cholesterol result of {} is considered {}"
+          .format(Chol_value, Chol_analy))
     return
+
 
 if __name__ == "__main__":
     interface()
--- dosing.py
+++ dosing.py
@@ -5,6 +5,7 @@
              type of medical treatment or diagnostics.
 """
 
+
 def dose_amount():
     print("Day One Dosing Guidelines")
     print("")
@@ -34,15 +35,17 @@ def dose_amount():
         weight = patient_weight_parse(weight_input)
         dosage = patient_dosage(weight, 4)
         output(weight, dosage)
-    return 
-    
+    return
+
+
 def patient_weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
     return weight_input
-    
+
+
 def patient_weight_parse(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
@@ -50,13 +53,15 @@ def patient_weight_parse(weight_input):
     if units == "lb":
         weight = weight / 2.205
     return weight
-    
+
+
 def patient_dosage(weight, diagnosis):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
     return dosage_mg_first_day
-    
+
+
 def output(weight, dosage):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
@@ -64,6 +69,6 @@ def output(weight, dosage):
           .format(dosage))
     return
 
+
 if __name__ == '__main__':
     dose_amount()
-
